bab8f2e1b98632be41540d5d319bd4fdc27b2db6
abhi587 <abhishek.pawan14@gmail.com>
2023-09-18 14:55:19 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+// const functions = require("firebase-functions")
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb+srv://abhishekprasad:abhiprasad@cluster0.ygncry8.mongodb.net/blogSite",
@@ -19,3 +22,5 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+// exports.api = functions.https.onRequest(app)
